00a820012d0de3ce148ec8bc8386a417cf85a4c6
Dharmik_403 <64529217+Dharmik48@users.noreply.github.com>
2022-08-14 15:19:47 +0530

    fix: adds the firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDm41fvYdUxvXIukT2_Erviy1kORDgFa-g",
+  authDomain: "notes-react-firebase-v1.firebaseapp.com",
+  projectId: "notes-react-firebase-v1",
+  storageBucket: "notes-react-firebase-v1.appspot.com",
+  messagingSenderId: "92720366633",
+  appId: "1:92720366633:web:5816f19b773c35ef540838"
+};
+
+export default firebaseConfig
