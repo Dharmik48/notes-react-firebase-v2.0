5fc728e0dee9d439cb16d0c163b86e58254e6ba9
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-08-18 22:21:33 +0530

    styled Login/Logout button

--- src/components/Account.js
+++ src/components/Account.js
@@ -24,14 +24,15 @@ const Account = ({ user, setUser, setNotes, setCreateNote, setSearchTerm }) => {
 				<img src={user.photoURL} alt="avatar" />
 			</div>
 			<p className="user-name">{user.displayName}</p>
-			<p style={{ marginLeft: '2.5em' }} onClick={signout}>
-				Sign Out
+			<p style={{ cursor: 'pointer', display: "flex", boxShadow:"2px 3px #bcb9b9", borderRadius: "5px", padding: "5px"}} onClick={signout}>
+			<img style={{height: "30px", marginRight: "8px"}} className='googleIcon' src={icon} alt='icon' />  
+			<span style={{marginTop: "5px"}}>Log out</span>
 			</p>
 		</div>
 	) : (
-		<p onClick={perfromLogin} style={{ cursor: 'pointer', display: "flex", boxShadow:"3px 5px #888888", borderRadius: "5px", border: "0.2px solid #888888", padding: "5px"}}>
-			<img style={{height: "30px"}} className='googleIcon' src={icon} alt='icon' />  
-			<span style={{marginTop: "5px"}}>Login with Google</span>
+		<p onClick={perfromLogin} style={{ cursor: 'pointer', display: "flex", boxShadow:"2px 3px #bcb9b9", borderRadius: "5px", padding: "5px"}}>
+			<img style={{height: "30px", marginRight: "8px"}} className='googleIcon' src={icon} alt='icon' />  
+			<span style={{marginTop: "5px"}}>Log in</span>
 		</p>
 	)
 }
